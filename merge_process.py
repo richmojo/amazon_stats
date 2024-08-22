851339b0482174b62885006f7ea5b4ced02262a8
Josh Richmond <jrichmond2009@gmail.com>
2024-08-22 11:49:22 -0400

    Add merge_a2a_product_data_batch function to db.py and update merge_process function in merge_process.py

--- merge_process.py
+++ merge_process.py
@@ -1,12 +1,15 @@
-from db import merge_product_data_batch
+from db import merge_product_data_batch, merge_a2a_product_data_batch
 import time
 
 
 def merge_process():
     while True:
-        response = merge_product_data_batch()
-        if not response:
+        products_response = merge_product_data_batch()
+        a2a_response = merge_a2a_product_data_batch()
+
+        if not products_response and not a2a_response:
             time.sleep(60)
+
         time.sleep(1)
 
 
